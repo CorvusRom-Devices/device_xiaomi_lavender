240d4454fa94d274264e27d66f31f8bbb9edd292
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-06 20:30:39 +0900

    lavender: init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    [edit: Use property_override() for overriding props]
    (Original commit is here: https://gerrit.aospa.co/c/AOSPA/android_system_core/+/4190)
    
    Change-Id: I3b7f20f1116318d1658e5bbc2694273e39afaaa5
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- init/init_lavender.cpp
+++ init/init_lavender.cpp
@@ -106,3 +106,44 @@ void vendor_load_properties()
     // Misc
     property_override("ro.apex.updatable", "false");
 }
+
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *cts_prop_key[] = {
+	"ro.boot.verifiedbootstate",
+	"ro.boot.flash.locked",
+	"ro.boot.selinux",
+	"ro.boot.veritymode",
+	"ro.boot.warranty_bit",
+	"ro.warranty_bit",
+	"ro.debuggable",
+	"ro.secure",
+	"ro.build.type",
+	"ro.build.tags",
+	"ro.build.selinux",
+	NULL
+};
+
+static const char *cts_prop_value[] = {
+	"green",
+	"1",
+	"enforcing",
+	"enforcing",
+	"0",
+	"0",
+	"0",
+	"1",
+	"user",
+	"release-keys",
+	"1",
+	NULL
+};
+
+static void workaround_cts_properties() {
+	// Hide all sensitive props
+	for (int i = 0; cts_prop_key[i]; ++i) {
+		property_override(cts_prop_key[i], cts_prop_value[i]);
+	}
+}
+
+/* Workaround CTS */
+    workaround_cts_properties();
