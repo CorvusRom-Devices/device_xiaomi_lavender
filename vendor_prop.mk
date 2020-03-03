d751662490d26f8c301583640c2cb85d828b98a1
tangrobin <tangrobin@google.com>
2020-03-03 17:14:25 +0800

    lavender: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 143933518
    Bug: 142222407
    Test: scrolling, app launching/closing, playing video
    
    Signed-off-by: MukulSharma <sharmamukul06175@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -123,6 +123,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.latch_unsignaled=0 \
     ro.opengles.version=196610 \
     ro.vendor.display.sensortype=2 \
     vendor.gralloc.enable_fb_ubwc=1 \
@@ -197,6 +198,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=1
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
