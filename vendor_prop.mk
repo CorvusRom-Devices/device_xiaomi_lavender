27a0baba46423222b58d760e6ca45f674267962e
Palakurthi Sreekanth <sreekfreak995@gmail.com>
2020-10-06 20:13:30 +0530

    lavender: enable A11 live blur by default
    * Also add props to make it smoother.
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -269,3 +269,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #disable dual isp sync by default
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.camera.dual.isp.sync=0
+
+#Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
