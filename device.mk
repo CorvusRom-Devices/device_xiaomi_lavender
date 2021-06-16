2c1049e15b012fc0c50e1e9c32bcc3eb15d88d66
Lucchetto <lucchetto.tie@live.com>
2021-06-16 09:04:10 +0530

    lavender: Create dummy libqti-perfd-client
    - proprietary perfd blobs can finally be nuked without breaking goodix
    - we could even map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -318,6 +318,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perfd (dummy)
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
      android.hardware.power-service.lavender-libperfmgr
