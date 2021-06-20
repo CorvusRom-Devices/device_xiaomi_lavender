f4432609fe800b094dd7019443334a4f863c95bc
sohamxda7 <sensoham135@gmail.com>
2021-06-20 05:24:41 +0000

    lavender : overlay changes for pixelplus
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-ppui
 
 # RRO configuration
 PRODUCT_ENFORCE_RRO_TARGETS := *
