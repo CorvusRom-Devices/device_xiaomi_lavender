102891d6b6925b1917632d049feb73a8488163bd
SayanAcharya <SayanAcharya@users.noreply.github.com>
2021-07-13 06:12:56 +0200

    lavender: Build `vendor.display.config@1.0.vendor`
    • dependency of liboemcrypto
    
    Change-Id: I9f5a5d97e71d6a9630dbea8accc752ae9b8bee77
    
    Signed-off-by: mukulsharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -200,6 +200,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0-impl \
     android.hardware.graphics.allocator@4.0-impl \
     android.hardware.memtrack@1.0-impl \
+    vendor.display.config@1.0.vendor \
     android.hardware.memtrack@1.0-service
 
 # DRM
