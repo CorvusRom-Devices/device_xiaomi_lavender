7a70bf702b006f3880ed2a9b02210389bb1f0a6f
Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
2021-08-22 11:37:29 +0530

    lavender:Switch to pixel chargin

--- corvus_lavender.mk
+++ corvus_lavender.mk
@@ -14,6 +14,7 @@ $(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Corvus build type
 CORVUS_MAINTAINER := Mukul_Sharma
