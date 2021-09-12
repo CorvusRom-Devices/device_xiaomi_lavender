f5ebd0a787ce315a637f016a9a2c2bd47bb55db0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:44:17 +0200

    lavender: libhidl: Move to Android.bp
    * Make the lib vendor_available and remove it from vndk-ext
    
    Change-Id: I3945a460d563aaf07aaa7b757bb9c0dddc4f91cd
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>
    Signed-off-by: Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>

--- device.mk
+++ device.mk
@@ -258,7 +258,7 @@ PRODUCT_BOOT_JARS += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
