c165ae6f50ed96f13c1ba0a340e9c3267eae65af
Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
2021-09-13 15:23:11 +0530

    address overlay-directory

--- device.mk
+++ device.mk
@@ -21,6 +21,8 @@ $(call inherit-product-if-exists, vendor/XiaomiParts/xiaomiparts.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-corvus \
+    $(LOCAL_PATH)/overlay-lineage
 
 # RRO configuration
 PRODUCT_ENFORCE_RRO_TARGETS := *
