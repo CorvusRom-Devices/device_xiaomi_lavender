50ea5d658b91a003991263be6c6205937fa0f146
Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
2021-10-06 19:35:46 +0530

    lavender: update redfin build fingerprint

--- corvus_lavender.mk
+++ corvus_lavender.mk
@@ -33,6 +33,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
 
-BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
