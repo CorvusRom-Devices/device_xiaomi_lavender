d87ddf954e3290f56bdc0cb42fae57034e0ab91b
faham1997 <nafidfaham08@gmail.com>
2021-10-08 19:53:47 +0600

    lavneder: Drop unused perf profile *not required in EAS
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -324,9 +324,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
