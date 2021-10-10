7b256548969a783a499f375af88cce0412d9e5cf
faham1997 <nafidfaham08@gmail.com>
2021-10-11 02:13:45 +0600

    lavender: build vendor variant of libqdMetaData
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     libdisplayconfig \
     libqdMetaData \
-    libqdMetaData.system \
+    libqdMetaData.vendor \
     libtinyxml \
     libtinyxml.vendor \
     vendor.display.config@1.9
