64cdc4551c1db534405d66649b95bd9e60e4d9f7
faham1997 <nafidfaham08@gmail.com>
2021-10-11 02:01:46 +0600

    lavender: Add pixel power hal to soong namespaces
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -27,7 +27,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
+                            hardware/google/interfaces \
+                            hardware/google/pixel
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
