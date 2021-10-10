81375cbc89fb04b2a407f94fb42d8437f7ebf86d
faham1997 <nafidfaham08@gmail.com>
2021-10-11 02:23:28 +0600

    lavender: Create empty hidl device makefile and inherit
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -16,6 +16,7 @@ TARGET_GAPPS_ARCH := arm64
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lavender
