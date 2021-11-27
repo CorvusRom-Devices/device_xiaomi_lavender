57220f9cf6ab4eae95e956fb9c1a97aee1a93fd5
Kunmun <kunmun.devroms@gmail.com>
2021-11-27 11:42:03 +0530

    lavender: configs: media: Checkout codecs performance to LA.UM.10.2.1.r1-02900-sdm660.0
    Change-Id: Ic9399fb17f62fefe00f6a80c8d64b9d29283a88f
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: RealAkira <beastdark704@gmail.com>
    
    lavender: config.fs: Checkout to LA.UM.10.2.1.r1-03200-sdm660.0
    
    Signed-off-by: mukulsharma06175 <sharmamukul06175@gmail.com>
    
    lavender: overlay: Checkout CarrierConfig to LA.UM.10.2.1.r1-03200-sdm660.0
    
    Change-Id: I441da73061bb970c5e7b3b5f320e554200059338
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: RealAkira <beastdark704@gmail.com>

--- config.fs
+++ config.fs
@@ -19,6 +19,24 @@ value:2906
 [AID_VENDOR_THERMAL]
 value:2907
 
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
+
+[vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[system/vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
@@ -30,6 +48,24 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/bin/cnss-daemon]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
 mode: 0755
@@ -43,6 +79,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE SYS_BOOT
 
+[system/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
@@ -50,6 +92,12 @@ group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -91,6 +139,12 @@ user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
@@ -126,6 +180,12 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/sensors.qti]
 mode: 0755
