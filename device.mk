58ad2e8932a5e54c2b1fad73c75b4e9f95a51c21
RealAkira <beastdark704@gmail.com>
2022-02-24 11:27:48 +0530

    lavender: rro_overlays: Drop TetheringOverlay
    * It is forced to use bpf in Android S. Also we have it backported in k4.4, so this overlay is totally useless now
    
    Signed-off-by: mukulsharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -440,10 +440,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext \
