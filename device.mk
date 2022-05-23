581a91ba3443f78e7f4dca9be2cc8058c2f23d11
MukulSharma06175 <sharmamukul06175@gmail.com>
2022-05-23 12:16:26 +0530

    lavender: Whitelist/Permissions: Add permissions for Carrier Settings - As I'm now using extracted google carrier settings override for a better support for user's around world, we need to give proper permission to package in order to all work well. - Used log:   11-01 23:58:45.236  1573  1573 I PackageManager: Permission ownership changed. Updating all permissions.   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission android.permission.CHANGE_COMPONENT_ENABLED_STATE for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission com.android.permission.INSTALL_EXISTING_PACKAGES for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission android.permission.INTERACT_ACROSS_USERS for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission android.permission.MODIFY_PHONE_STATE for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission android.permission.READ_PRIVILEGED_PHONE_STATE for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist   11-01 23:58:45.248  1573  1573 W PackageManager: Privileged permission android.permission.WRITE_APN_SETTINGS for package com.google.android.carrier (/system/product/priv-app/CarrierSettings) not in privapp-permissions allowlist - References:   -https://gitlab.com/PixelExperience/vendor_gapps   -https://github.com/hentaiOS/platform_vendor_hentai
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- device.mk
+++ device.mk
@@ -389,7 +389,9 @@ PRODUCT_PACKAGES += \
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(DEVICE_PATH)/configs/google-hiddenapi-package-whitelist-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist-carriersettings.xml \
+    $(DEVICE_PATH)/configs/privapp-permissions-google-p-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-p-carriersettings.xml
 
 # QMI
 PRODUCT_PACKAGES += \
